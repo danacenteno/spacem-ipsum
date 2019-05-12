ab1f4dd17ffa4840d8a22649b8d5dabce1f57423
Dana Centeno <danacentenodes@gmail.com>
2019-05-12 00:26:01 -0400

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Create an empty array
+// Create an array with the words/phrases you want to use
+
+// 
+
+// On submit, remove class of '.hiddenText' to reveal generated text (fades in)
+
+// User can copy generated text by clicking on the '.copyMe' button
+
+// User clicks on button to clear existing text and choose again
