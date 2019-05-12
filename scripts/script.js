4146826f9d6ceed6e2088361e0112b46a1ab1716
Dana Centeno <danacentenodes@gmail.com>
2019-05-12 12:53:09 -0400

    Updated html and scss - added some JS in there

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,32 @@
-// Create an empty array
-// Create an array with the words/phrases you want to use
+// Create an empty object to push things into
+const spacem = {};
 
-// 
+spacem.words = [
+    "This sentence one lalalalalalala",
+    "This sentence two lalalalalalala",
+    "This sentence three lalalalalalala",
+    "This sentence four lalalalalalala",
+    "This sentence five lalalalalalala",
+    "This sentence six lalalalalalala",
+    "This sentence seven lalalalalalala",
+    "This sentence eight lalalalalalala"
+];
+
+// Create an array with the words/phrases you want to use
+const paragraph = ['Spacem ipsum '];
+const generatedText = [];
+
+function formSentences() {
+	spacem.words.sort(function () {
+		return 0.1 - Math.random();
+	});
+	var sentences = spacem.words.slice(0, 3).join(' ') + '. ';
+	sentences = sentences.charAt(0).toUpperCase() + sentences.slice(1);
+    paragraph.push(sentences);
+    
+    console.log(sentences);
+}
+
 
 // On submit, remove class of '.hiddenText' to reveal generated text (fades in)
 
